a084a6c6d70ca8da8bb6e3e73d9f23f6d04fa394
Simon Roth <systats@users.noreply.github.com>
2018-03-12 19:11:43 +0100

    new datasets for text mining

---
+++ R/export_data.R
@@ -0,0 +1,57 @@
+# Data and dictionaries for text normalization
+
+#' German stopwords
+#'
+#' @format A data frame with 358473 rows and 2 variables:
+#' \describe{
+#'   \item{word}{a key word}
+#'   \item{lemma}{its mapped lemma.}
+#'   ...
+#' }
+#' @source \url{http://www.lexiconista.com/datasets/lemmatization/}
+"stop_words_de"
+
+#' English stopwords
+#'
+#' @format A data frame with 358473 rows and 2 variables:
+#' \describe{
+#'   \item{word}{a key word}
+#'   \item{lemma}{its mapped lemma.}
+#'   ...
+#' }
+#' @source \url{http://www.lexiconista.com/datasets/lemmatization/}
+"stop_words_en"
+
+
+#' German Lemma Dictionary
+#'
+#' @format A data frame with 358473 rows and 2 variables:
+#' \describe{
+#'   \item{word}{a key word}
+#'   \item{lemma}{its mapped lemma.}
+#'   ...
+#' }
+#' @source \url{http://www.lexiconista.com/datasets/lemmatization/}
+"hash_lemma_de"
+
+#' English Lemma Dictionary
+#'
+#' @format A data frame with 41759 rows and 2 variables:
+#' \describe{
+#'   \item{word}{a key word}
+#'   \item{lemma}{its mapped lemma.}
+#'   ...
+#' }
+#' @source \url{http://www.lexiconista.com/datasets/lemmatization/}
+"hash_lemma_en"
+
+#' French Lemma Dictionary
+#'
+#' @format A data frame with 224001 rows and 2 variables:
+#' \describe{
+#'   \item{word}{a key word}
+#'   \item{lemma}{its mapped lemma.}
+#'   ...
+#' }
+#' @source \url{http://www.lexiconista.com/datasets/lemmatization/}
+"hash_lemma_fr"
