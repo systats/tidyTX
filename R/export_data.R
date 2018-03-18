f71047159ef8ccdf4b840c93d098aa67518dbc76
Simon Roth <systats@users.noreply.github.com>
2018-03-18 21:31:20 +0100

    adding three metric dictionaries

--- R/export_data.R
+++ R/export_data.R
@@ -55,3 +55,46 @@
 #' }
 #' @source \url{http://www.lexiconista.com/datasets/lemmatization/}
 "hash_lemma_fr"
+
+
+
+#' Wordnet Sentiment Dictionary
+#'
+#' @format A data frame with 24,428 rows and 3 variables:
+#' \describe{
+#'   \item{word}{a key word}
+#'   \item{wordnet_score}{ sentiment number }
+#'   \item{wordnet_class}{ sentiment class }
+#'   ...
+#' }
+#' @source \url{http://www.lexiconista.com/datasets/lemmatization/}
+"sent_wordnet"
+
+
+#' Vader Sentiment Dictionary
+#'
+#' @format A data frame with 7,516 rows and 6 variables:
+#' \describe{
+#'   \item{word}{a key word}
+#'   \item{vader_score}{ sentiment number }
+#'   \item{vader_intens}{ sentiment number }
+#'   \item{vader_seq}{ sentiment number }
+#'   \item{vader_med}{ sentiment number }
+#'   \item{vader_class}{ sentiment class }
+#'   ...
+#' }
+#' @source \url{http://www.lexiconista.com/datasets/lemmatization/}
+"sent_vader"
+
+
+#' AFINN Sentiment Dictionary
+#'
+#' @format A data frame with 2,476 rows and 2 variables:
+#' \describe{
+#'   \item{word}{a key word}
+#'   \item{afinn_score}{ sentiment number }
+#'   ...
+#' }
+#' @source \url{http://www.lexiconista.com/datasets/lemmatization/}
+"sent_afinn"
+
